749f9bb60d1e763e652bdd9981a6602dbbe22216
VivianAllen <mrvivianallen@gmail.com>
2018-02-12 15:48:08 +0000

    Exercise 7 completed, dynamic student and cohort input

--- directory.rb
+++ directory.rb
@@ -1,16 +1,56 @@
-def input_students
-  puts 'Please enter the names of the students'
-  puts 'To finish, just hit return twice'
-  # create an empty array
-  students = []
-  # get the first name
-  name = gets.chomp
-  # while the name is not empty, repeat this vode
-  while !name.empty? do
-    # add to hash, all november cohort
-    students << { name: name, cohort: :november }
-    puts "Now we have #{students.count} students"
+def input_name
+  blanks = 0
+  loop do
+    case blanks
+    when 1
+      puts 'enter blank name again to quit'
+    when 2
+      return nil
+    end
+    puts 'Please enter students name'
     name = gets.chomp
+    if name.empty?
+      blanks += 1
+    else
+      puts "You entered #{name}. Is that correct? y/n"
+      input = gets.chomp
+      until %w[y n].include?(input)
+        puts 'Please enter y or n'
+        input = gets.chomp
+      end
+      return name if input.casecmp('y').zero?
+    end
+  end
+end
+
+def input_cohort
+  loop do
+    puts "Please enter students cohort"
+    cohort = gets.chomp
+    if cohort.empty?
+      puts "If no input detected, default (November cohort) will be used"
+      cohort = 'November'
+    end
+    puts "You entered #{cohort}. Is that correct? y/n"
+    input = gets.chomp
+    until %w[y n].include?(input)
+      puts 'Please enter y or n'
+      input = gets.chomp
+    end
+    return cohort.to_sym if input.casecmp('y').zero?
+  end
+end
+
+def input_students
+  students = []
+  puts 'Please enter the students details'
+  puts 'To finish, just hit enter a blank name twice'
+  loop do
+    name = input_name
+    break if name.nil?
+    cohort = input_cohort
+    students << { name: name, cohort: cohort }
+    puts "Now we have #{students.count} students"
   end
   students
 end
@@ -22,12 +62,12 @@ end
 
 def print(students)
   students.each do |student|
-    puts "#{student[:name].center(20)} (#{student[:cohort]} cohort)"
+    puts "#{student[:name].center(20)} (#{student[:cohort].to_s.center(9)} cohort)"
   end
 end
 
-def print_footer(names)
-  puts "Overall, we have #{names.count} great students"
+def print_footer(students)
+  puts "Overall, we have #{students.count} great students"
 end
 students = input_students
 print_header
