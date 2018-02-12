5f36833504e1603fde0195fb3c852f91f966e6d2
VivianAllen <mrvivianallen@gmail.com>
2018-02-12 09:54:55 +0000

    student director as ary ary with cohorts as symbols

--- directory.rb
+++ directory.rb
@@ -1,23 +1,23 @@
 students = [
-  'The students of Villains Academy',
-  'Dr. Hannibal Lecter',
-  'Darth Vader',
-  'Nurse Ratched',
-  'Michael Corleone',
-  'Alex DeLarge',
-  'The Wicked Witch of the West',
-  'Terminator',
-  'Freddy Krueger',
-  'The Joker',
-  'Joffrey Baratheon',
-  'Norman Bates'
+  ['The students of Villains Academy', :november],
+  ['Dr. Hannibal Lecter', :november],
+  ['Darth Vader', :november],
+  ['Nurse Ratched', :november],
+  ['Michael Corleone', :november],
+  ['Alex DeLarge', :november],
+  ['The Wicked Witch of the West', :november],
+  ['Terminator', :november],
+  ['Freddy Krueger', :november],
+  ['The Joker', :november],
+  ['Joffrey Baratheon', :november],
+  ['Norman Bates', :november]
 ]
 def print_header
   puts 'The students of Villains Academy'
   puts '--------------'
 end
-def print(names)
-  names.each { |name| puts name }
+def print(students)
+  names.each { |student| puts "#{student[0]} (#{student[1]} cohort)" }
 end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
