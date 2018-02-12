b0ce92b21e9440fb0469112daaedf8cac17f2bc4
VivianAllen <mrvivianallen@gmail.com>
2018-02-12 13:24:09 +0000

    reset to beginning of exercises

--- directory.rb
+++ directory.rb
@@ -21,10 +21,8 @@ def print_header
 end
 
 def print(students)
-  print_i = 0
-  while print_i < students.length
-    puts "#{students[print_i][:name]} (#{students[print_i][:cohort]} cohort)"
-    print_i += 1
+  students.each do |student|
+    puts "#{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
