f34f59f0a0369e8becb93ac0b8a69245bc70c18d
VivianAllen <mrvivianallen@gmail.com>
2018-02-12 10:18:33 +0000

    added interactive student list generation

--- directory.rb
+++ directory.rb
@@ -1,17 +1,20 @@
-students = [
-  { name: 'The students of Villains Academy', cohort: :november },
-  { name: 'Dr. Hannibal Lecter', cohort: :november },
-  { name: 'Darth Vader', cohort: :november },
-  { name: 'Nurse Ratched', cohort: :november },
-  { name: 'Michael Corleone', cohort: :november },
-  { name: 'Alex DeLarge', cohort: :november },
-  { name: 'The Wicked Witch of the West', cohort: :november },
-  { name: 'Terminator', cohort: :november },
-  { name: 'Freddy Krueger', cohort: :november },
-  { name: 'The Joker', cohort: :november },
-  { name: 'Joffrey Baratheon', cohort: :november },
-  { name: 'Norman Bates', cohort: :november }
-]
+def input_students
+  puts 'Please enter the names of the students'
+  puts 'To finish, just hit return twice'
+  # create an empty array
+  students = []
+  # get the first name
+  name = gets.chomp
+  # while the name is not empty, repeat this vode
+  while !name.empty? do
+    # add to hash, all november cohort
+    students << { name: name, cohort: :november }
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
+
 def print_header
   puts 'The students of Villains Academy'
   puts '--------------'
@@ -24,6 +27,7 @@ end
 def print_footer(names)
   puts "Overall, we have #{names.count} great students"
 end
+students = input_students
 print_header
 print(students) # is this cool? print is already a built-in method
 print_footer(students)
