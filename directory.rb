5aad25013454acee790e2fe64d167c5cb9cb976a
VivianAllen <mrvivianallen@gmail.com>
2018-02-13 17:02:26 +0000

    Exercise 14_3, refactoring

--- directory.rb
+++ directory.rb
@@ -1,27 +1,31 @@
 @students = []
 
+def check_quit(name)
+  if name.empty?
+    puts 'Enter blank name again to quit, enter name to continue'
+    input = STDIN.gets.chomp
+    input.empty? ? false : input
+  else
+    name
+  end
+end
+
+def check_input(input)
+  puts "You entered #{input}. Is that correct?"
+  answer = ''
+  until %w[y n].include?(answer)
+    puts 'Please enter y or n'
+    answer = STDIN.gets.chomp
+  end
+  answer.casecmp('y').zero? ? true : false
+end
+
 def input_name
-  blanks = 0
   loop do
-    case blanks
-    when 1
-      puts 'enter blank name again to quit'
-    when 2
-      return nil
-    end
     puts 'Please enter students name'
-    name = STDIN.gets.chomp
-    if name.empty?
-      blanks += 1
-    else
-      puts "You entered #{name}. Is that correct? y/n"
-      input = STDIN.gets.chomp
-      until %w[y n].include?(input)
-        puts 'Please enter y or n'
-        input = STDIN.gets.chomp
-      end
-      return name if input.casecmp('y').zero?
-    end
+    name = check_quit(STDIN.gets.chomp)
+    return false unless name
+    return name if check_input(name)
   end
 end
 
@@ -33,13 +37,7 @@ def input_cohort
       puts 'If no input detected, default (November cohort) will be used'
       cohort = 'November'
     end
-    puts "You entered #{cohort}. Is that correct? y/n"
-    input = STDIN.gets.chomp
-    until %w[y n].include?(input)
-      puts 'Please enter y or n'
-      input = STDIN.gets.chomp
-    end
-    return cohort.to_sym if input.casecmp('y').zero?
+    return cohort if check_input(cohort)
   end
 end
 
@@ -47,18 +45,20 @@ def add_student(name, cohort)
   @students << { name: name, cohort: cohort.to_sym }
 end
 
+def count_students
+  student_string = @students.count > 1 ? 'students' : 'student'
+  puts "Now we have #{@students.count} #{student_string}"
+end
+
 def input_students
-  students = []
   puts 'Please enter the students details'
-  puts 'To finish, just hit enter a blank name twice'
+  puts 'To finish, enter a blank name twice'
   loop do
     name = input_name
-    break if name.nil?
+    return unless name
     add_student(name, input_cohort)
-    student_string = @students.count > 1 ? 'students' : 'student'
-    puts "Now we have #{@students.count} #{student_string}"
+    count_students
   end
-  students
 end
 
 def print_header
