6e9f1a965e5ccc5c98285db81441d081a401d75d
VivianAllen <mrvivianallen@gmail.com>
2018-02-14 10:32:21 +0000

    Exercise 14_7 completed, generic file handling replaced with CSV

--- directory.rb
+++ directory.rb
@@ -1,3 +1,4 @@
+require 'csv'
 @students = []
 
 def check_quit(name)
@@ -85,22 +86,18 @@ def filename_chooser
 end
 
 def save_students(filename)
-  File.open(filename, 'w') do |file|
+  CSV.open(filename, 'w') do |csv|
     @students.each do |student|
-      student_data = [student[:name], student[:cohort]]
-      csv_line = student_data.join(',')
-      file.puts csv_line
+      csv << [student[:name], student[:cohort]]
     end
   end
   puts "student data saved to #{filename}"
 end
 
 def load_students(filename)
-  File.open(filename, 'r') do |file|
-    file.readlines.each do |line|
-      name, cohort = line.chomp.split(',')
-      add_student(name, cohort)
-    end
+  CSV.foreach(filename) do |row|
+    name, cohort = row
+    add_student(name, cohort)
   end
   puts "student data loaded from #{filename}"
 end
